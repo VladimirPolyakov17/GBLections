d6a897642c10d5398d56007876f0ee36a3978cf3
Vladimir <vpolyakovup122@gmail.com>
2023-01-15 17:46:21 +0500

    Сложение рандомного числа

--- Examples/Example003_Sum/Program.cs
+++ Examples/Example003_Sum/Program.cs
@@ -1,6 +1,6 @@
-﻿int numberA = new Random ().Next(1, 11); // 1 2 3 4 ...10
+﻿int numberA = new Random ().Next(1, 12); // 1 2 3 4 ...11
 Console.WriteLine(numberA);
-int numberB = new Random ().Next(1, 11);
+int numberB = new Random ().Next(1, 12);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);

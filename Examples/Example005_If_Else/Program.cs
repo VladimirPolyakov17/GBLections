9374ac06ac0fff84157934a67daed929f6590437
Vladimir <vpolyakovup122@gmail.com>
2023-01-15 18:50:19 +0500

    Если,то

---
+++ Examples/Example005_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Аня")
+{
+  Console.WriteLine("Ура, это же Аня");
+}
+else
+{
+  Console.Write("Привет,");
+  Console.WriteLine(username);
+} 
